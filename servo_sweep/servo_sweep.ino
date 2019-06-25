e03c90a3cb788eb84b5c1ae82456d96a79fbee01
Rusty Eddy <rusty@rustyeddy.com>
2019-06-24 17:04:33 -0700

    upload code

--- servo_sweep/servo_sweep.ino
+++ servo_sweep/servo_sweep.ino
@@ -10,21 +10,27 @@
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-
 int pos = 0;    // variable to store the servo position
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(11520);
+  myservo.attach(11);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+  // Position 0
+  Serial.println("setting position 0");
+  myservo.write(0);
+  delay(2000);
+
+  for (pos = 0; pos <= 120; pos += 1) { // goes from 0 degrees to 180 degrees
+    Serial.println(pos);
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+  for (pos = 120; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    Serial.println(pos);
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
